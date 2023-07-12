3521dbfa288f4baef0427b169a7b6676e2df8453
Bilal Issmail <bilal.issmail@gmail.com>
2023-07-12 11:20:03 +0300

    1st commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Choice 1: Count Digits
+def countDigits(num):
+  if num < 10:
+    return(1)
+  else:
+    return(1 + countDigits(num//10))
+num = int(input("Enter an integer:"))
+print(countDigits(num))  
+
+
+# Choice 2: Find max
+def findMax():
+  for i in range(len(list)):
+    if list[i] > list[i+1]:
+      
