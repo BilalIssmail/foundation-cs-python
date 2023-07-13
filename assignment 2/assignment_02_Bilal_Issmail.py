1d7535dcb8a43262cb927ba3ebbad59ff3f7ef36
BilalIssmail <138321132+BilalIssmail@users.noreply.github.com>
2023-07-13 23:47:01 +0300

    Update assignment_02_Bilal_Issmail.py
    Added the first to choice to a main display menu function.

--- assignment 2/assignment_02_Bilal_Issmail.py
+++ assignment 2/assignment_02_Bilal_Issmail.py
@@ -1,31 +1,35 @@
-# Choice 1: Count Digits
-def countDigits(num):
-  if num < 10:
-    return(1)
+def displayMenu():
+  choice = int(input("""Choose:
+  1- Count Digits
+  2- Find Maximum
+  3- Exit"""))
+  if choice == 1:
+    def countDigits(num):
+     if num < 10:
+       return(1)
+     else:
+       return(1 + countDigits(num//10))
+    num = int(input("Enter an integer:"))
+    print(countDigits(num))
+  elif choice == 2:
+    list = []
+    n = int(input("Enter an integer to add to the list or press zero to end list and find the maximum:"))
+    while  n != 0:
+      list.append(n)
+      n = int(input("Enter an integer to add to the list or press zero to end list and find the maximum:"))
+    else:
+      print("The integers you've entered are "+ str(list))
+
+    def findMax(list):
+     if len(list) > 1:
+      if list[0] > list[1]:
+       del(list[1])
+      else:
+       del(list[0])
+      findMax(list)
+     else:
+      print("The max integer is " + str(list) )
+    findMax(list)
   else:
-    return(1 + countDigits(num//10))
-num = int(input("Enter an integer:"))
-print(countDigits(num))  
-
-
-# Choice 2: Find max
-list = []
-n = int(input("Enter an integer to add to the list or press zero to end list and find the maximum:"))
-while  n != 0:
-  list.append(n)
-  n = int(input("Enter an integer to add to the list or press zero to end list and find the maximum:"))
-else:
- print("The integers you've entered are "+ str(list))
-
-def findMax(list):
- if len(list) > 1:
-  if list[0] > list[1]:
-   del(list[1])
-  else:
-   del(list[0])
-  findMax(list)
- else:
-  print("The max integer is " + str(list) )
-findMax(list)
-  
-      
+   print("error")
+displayMenu()
